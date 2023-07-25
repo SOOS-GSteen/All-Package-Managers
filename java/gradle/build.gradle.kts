d2380cbb43483975e6f3427e570993bf149c0fe1
SOOS-FJuarez <113910660+SOOS-FJuarez@users.noreply.github.com>
2023-07-25 15:01:19 -0300

    Add files via upload

---
+++ java/gradle/build.gradle.kts
@@ -0,0 +1,17 @@
+﻿rootProject.name = 'manifest-gradle-kts'
+version = "0.1"
+
+plugins {
+    id("demo.kotlin-application-conventions")
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-text:1.9")
+    implementation("org.apache.logging.log4j:log4j-core:2.16.0")
+    testImplementation("junit:junit:4.+")
+	implementation("org.apache.ant:ant:1.10.7")
+}
+
+application {
+    mainClass.set("demo.app.AppKt")
+}
